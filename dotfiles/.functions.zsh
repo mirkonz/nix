5656c9692d8db94544c4a0f5801c70c13c10cba8
Mirko May <hi@mirko.nz>
2025-05-19 17:22:14 +1200

    Updated aliases and scripts

--- dotfiles/.functions.zsh
+++ dotfiles/.functions.zsh
@@ -4,28 +4,98 @@ fg[brown]='\033[38;5;130m'
 
 function random_icon() {
     local icons=(
-        "$fg[green]\ue2a2$reset_color" # Palm tree
-        "$fg[brown]\uf0f4$reset_color" # Coffee
-        "$fg[yellow]\ude4a$reset_color" # Fingers crossed
-        "$fg[red]\uf5fd$reset_color" # Fire
-        "$fg[green]\uf55c$reset_color" # Alien
-        "$fg[white]\uf313$reset_color" # Moon
-        "$fg[yellow]\uf314$reset_color" # Sun
-        "$fg[blue]\uf327$reset_color" # Rocket
-        "$fg[blue]\uf435$reset_color" # Dog
-        "$fg[blue]\uf6a0$reset_color" # Cat
-        "$fg[white]\uf6d3$reset_color" # Cow
-        "$fg[green]\uf6c3$reset_color" # Frog
-        "$fg[black]\uf700$reset_color" # Crow
-        "$fg[cyan]\uf11b$reset_color" # Gamepad
-        "$fg[red]\uf2c8$reset_color" # Thermometer
-        "$fg[cyan]\uf233$reset_color" # Server
-        "$fg[cyan]\uf108$reset_color" # Computer
-        "$fg[yellow]\uf6de$reset_color" # Cheese
-        "$fg[yellow]\uf6d0$reset_color" # Pizza
-        "$fg[magenta]\uf6cc$reset_color" # Ice Cream
-        "$fg[white]\uf75f$reset_color" # Skull
-        "$fg[red]\uf4a5$reset_color" # Explosion
+        "$fg[black]$reset_color"
+        "$fg[black]$reset_color"
+        "$fg[black]ﮏ$reset_color"
+        "$fg[black]礼$reset_color"
+        "$fg[black]$reset_color"
+        "$fg[black]$reset_color"
+        "$fg[black]$reset_color"
+
+        "$fg[blue]$reset_color"
+        "$fg[blue]$reset_color"
+        "$fg[blue]$reset_color"
+        "$fg[blue]﫶$reset_color"
+        "$fg[blue]$reset_color"
+        "$fg[blue]$reset_color"
+        "$fg[blue]$reset_color"
+
+        "$fg[brown]$reset_color"
+        "$fg[brown]$reset_color"
+        "$fg[brown]$reset_color"
+        "$fg[brown]$reset_color"
+
+        "$fg[cyan]$reset_color"
+        "$fg[cyan]$reset_color"
+        "$fg[cyan]$reset_color"
+        "$fg[cyan]$reset_color"
+        "$fg[cyan]$reset_color"
+        "$fg[cyan]$reset_color"
+        "$fg[cyan]$reset_color"
+
+        "$fg[green]$reset_color"
+        "$fg[green]$reset_color"
+        "$fg[green]$reset_color"
+        "$fg[green]$reset_color"
+        "$fg[green]$reset_color"
+        "$fg[green]$reset_color"
+        "$fg[green]$reset_color"
+        "$fg[green]$reset_color"
+        "$fg[green]滑$reset_color"
+        "$fg[green]$reset_color"
+        "$fg[green]$reset_color"
+
+        "$fg[magenta]$reset_color"
+        "$fg[magenta]$reset_color"
+        "$fg[magenta]$reset_color"
+        "$fg[magenta]$reset_color"
+        "$fg[magenta]$reset_color"
+        "$fg[magenta]$reset_color"
+        "$fg[magenta]豈$reset_color"
+
+        "$fg[red]$reset_color"
+        "$fg[red]$reset_color"
+        "$fg[red]$reset_color"
+        "$fg[red]$reset_color"
+        "$fg[red]異$reset_color"
+        "$fg[red]$reset_color"
+        "$fg[red]$reset_color"
+        "$fg[red]$reset_color"
+        "$fg[red]$reset_color"
+        "$fg[red]$reset_color"
+        "$fg[red]$reset_color"
+        "$fg[red]$reset_color"
+        "$fg[red]嗢$reset_color"
+        "$fg[red]$reset_color"
+
+        "$fg[white]$reset_color"
+        "$fg[white]$reset_color"
+        "$fg[white]$reset_color"
+        "$fg[white]$reset_color"
+        "$fg[white]$reset_color"
+        "$fg[white]$reset_color"
+        "$fg[white]$reset_color"
+        "$fg[white]$reset_color"
+        "$fg[white]$reset_color"
+
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
+        "$fg[yellow]$reset_color"
     )
     echo -e "${icons[$RANDOM % ${#icons[@]}]}"
 }
--- home.nix
+++ home.nix
@@ -7,138 +7,143 @@
 
 let
   packages = import ./packages.nix { inherit pkgs; };
-  safariWebAppCommands = lib.concatStringsSep "\n" (map (app:
-    ''
-    ~/Library/Mobile\ Documents/com~apple~CloudDocs/Scripts/create-safari-webapp.applescript "${app.url}" "${app.name}"
-    ''
-  ) packages.safariWebApps);
+  safariWebAppCommands = lib.concatStringsSep "\n" (
+    map (app: ''
+      ~/Library/Mobile\ Documents/com~apple~CloudDocs/Scripts/create-safari-webapp.applescript "${app.url}" "${app.name}"
+    '') packages.safariWebApps
+  );
 in
 {
   # Home Manager settings
-  home-manager.users.${username} = { pkgs, lib, ...}: {
-    home.username = username;
-    home.homeDirectory = "/Users/${username}";
-    home.stateVersion = "23.11";
+  home-manager.users.${username} =
+    { pkgs, lib, ... }:
+    {
+      home.username = username;
+      home.homeDirectory = "/Users/${username}";
+      home.stateVersion = "23.11";
 
-    home.sessionVariables = {
-      ZDOTDIR = "$HOME";
-      EDITOR = "code";
-    };
-
-    # Home Manager managed packages
-    home.packages = packages.userPackages;
-
-    # Dotfiles
-    home.file = {
-      ".gitconfig".source = ./dotfiles/.gitconfig;
-      ".gitignore_global".source = ./dotfiles/.gitignore_global;
-      ".hushlogin".source = ./dotfiles/.hushlogin;
-      ".npmrc".source = ./dotfiles/.npmrc;
-      ".p10k.zsh".source = ./dotfiles/.p10k.zsh;
-      ".vimrc".source = ./dotfiles/.vimrc;
-      ".functions.zsh".source = ./dotfiles/.functions.zsh;
-      ".zinit.zsh".source = ./dotfiles/.zinit.zsh;
-    };
-
-    programs = {
-      home-manager.enable = true;
-
-      git = {
-        enable = true;
-        userName = "Mirko May";
-        userEmail = "hi@mirko.nz";
+      home.sessionVariables = {
+        ZDOTDIR = "$HOME";
+        EDITOR = "code";
       };
 
-      direnv = {
-        enable = true;
-        enableZshIntegration = true;
-        nix-direnv.enable = true;
+      # Home Manager managed packages
+      home.packages = packages.userPackages;
+
+      # Dotfiles
+      home.file = {
+        ".gitconfig".source = ./dotfiles/.gitconfig;
+        ".gitignore_global".source = ./dotfiles/.gitignore_global;
+        ".hushlogin".source = ./dotfiles/.hushlogin;
+        ".npmrc".source = ./dotfiles/.npmrc;
+        ".p10k.zsh".source = ./dotfiles/.p10k.zsh;
+        ".vimrc".source = ./dotfiles/.vimrc;
+        ".functions.zsh".source = ./dotfiles/.functions.zsh;
+        ".zinit.zsh".source = ./dotfiles/.zinit.zsh;
       };
 
-      fzf = {
-        enable = true;
-        enableZshIntegration = true;
-      };
+      programs = {
+        home-manager.enable = true;
 
-      # Zsh configuration
-      zsh = {
-        enable = true;
-        enableCompletion = true;
-        autosuggestion.enable = true;
-        syntaxHighlighting.enable = true;
-        shellAliases = {
-          # reload = "source ~/.zshrc";
-          reload = "exec zsh; echo -e \"$(random_icon)  Done!\"";
-          edit = "$EDITOR $HOME/nix";
-          hosts = "sudo $EDITOR /etc/hosts";
-
-          nixup = "nix run nix-darwin -- switch --flake .#mirko";
-
-          l = "eza -bGF --header --git --color=always --group-directories-first --icons";
-          ls = "eza --header --git --color=always --group-directories-first --icons";
-          ll = "eza -la --header --git --icons --octal-permissions --group-directories-first";
-          llm = "eza -lbGd --header --git --sort=modified --color=always --group-directories-first --icons";
-          la = "eza --long --all --group --group-directories-first";
-          lx = "eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --color=always --group-directories-first --icons";
-
-          lS = "eza -1 --color=always --group-directories-first --icons";
-          lt = "eza --tree --level=2 --color=always --group-directories-first --icons";
-          # l. = "eza -a | grep -E '^\.'";
-
-          t = "eza -Tl --icons";
-          tree = "eza -T --icons";
-
-          vi = "nvim";
-          vim = "nvim";
-          _vim = "vi";
-
-          cat = "bat";
-
-          ytd = "yt-dlp";
+        git = {
+          enable = true;
+          userName = "Mirko May";
+          userEmail = "hi@mirko.nz";
         };
-        initContent = ''
-          # Functions
-          [[ ! -f ~/.functions.zsh ]] || source ~/.functions.zsh
 
-          # Greeting
-          echo -e "Hi ${username}!  $(random_icon)"
+        direnv = {
+          enable = true;
+          enableZshIntegration = true;
+          nix-direnv.enable = true;
+        };
 
-          # Powerlevel10k
-          source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-          [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+        fzf = {
+          enable = true;
+          enableZshIntegration = true;
+        };
 
-          # ZINIT
-          source ${pkgs.zinit}/share/zinit/zinit.zsh
-          [[ ! -f ~/.zinit.zsh ]] || source ~/.zinit.zsh
+        # Zsh configuration
+        zsh = {
+          enable = true;
+          enableCompletion = true;
+          autosuggestion.enable = true;
+          syntaxHighlighting.enable = true;
+          shellAliases = {
+            ".." = "cd ..";
+            "..." = "cd ../..";
+            "...." = "cd ../../..";
 
-          # FZF tab
-          source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+            l = "eza -bGF --header --git --color=always --group-directories-first --icons";
+            ls = "eza --header --git --color=always --group-directories-first --icons";
+            la = "eza --long --all --group --group-directories-first";
+            ll = "eza -la --header --git --icons --octal-permissions --group-directories-first";
 
-          # NVM NODE
-          [ -s "$(brew --prefix nvm)/nvm.sh" ] && \. "$(brew --prefix nvm)/nvm.sh"
-          [ -s "$(brew --prefix nvm)/etc/bash_completion.d/nvm" ] && \. "$(brew --prefix nvm)/etc/bash_completion.d/nvm"
-          autoload -U add-zsh-hook
-          load-nvmrc() {
-              if [[ -f .nvmrc && -r .nvmrc ]]; then
-                  nvm use
-              elif [[ $(nvm version) != $(nvm version default) ]]; then
-                  echo "Reverting to nvm default version"
-                  nvm use default
-              fi
-          }
-          add-zsh-hook chpwd load-nvmrc
-          load-nvmrc
+            t = "eza -Tl --icons";
+            tree = "eza -T --icons";
 
-          # RUBY
-          eval "$(rbenv init -)"
-        '';
+            gra = "git rebase abort";
+
+            grep = "rg";
+            rm = "rm -i";
+            cp = "cp -i";
+            mv = "mv -i";
+
+            vi = "nvim";
+            vim = "nvim";
+            _vim = "vi";
+
+            cat = "bat";
+
+            ytd = "yt-dlp";
+
+            reload = "exec zsh; echo -e \"$(random_icon)  Done!\"";
+            edit = "$EDITOR $HOME/nix";
+            hosts = "sudo $EDITOR /etc/hosts";
+            nixup = "nix run nix-darwin -- switch --flake .#mirko";
+          };
+          initContent = ''
+            # Functions
+            [[ ! -f ~/.functions.zsh ]] || source ~/.functions.zsh
+
+            # Greeting
+            echo -e "Hi ${username}!  $(random_icon)"
+
+            # Powerlevel10k
+            source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+            [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+            # ZINIT
+            source ${pkgs.zinit}/share/zinit/zinit.zsh
+            [[ ! -f ~/.zinit.zsh ]] || source ~/.zinit.zsh
+
+            # FZF tab
+            source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+
+            # NVM NODE
+            [ -s "$(brew --prefix nvm)/nvm.sh" ] && \. "$(brew --prefix nvm)/nvm.sh"
+            [ -s "$(brew --prefix nvm)/etc/bash_completion.d/nvm" ] && \. "$(brew --prefix nvm)/etc/bash_completion.d/nvm"
+            autoload -U add-zsh-hook
+            load-nvmrc() {
+                if [[ -f .nvmrc && -r .nvmrc ]]; then
+                    nvm use
+                elif [[ $(nvm version) != $(nvm version default) ]]; then
+                    echo "Reverting to nvm default version"
+                    nvm use default
+                fi
+            }
+            add-zsh-hook chpwd load-nvmrc
+            load-nvmrc
+
+            # RUBY
+            eval "$(rbenv init -)"
+          '';
+        };
       };
+
+      # Run the AppleScript for each web app
+      # home.activation.createSafariWebApps = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      #   ${safariWebAppCommands}
+      # '';
+
     };
-
-    # Run the AppleScript for each web app
-    # home.activation.createSafariWebApps = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    #   ${safariWebAppCommands}
-    # '';
-
-  };
 }
